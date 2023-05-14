ff6f8dd9c38f2726af1693bf018363bd396ce365
mfikriam <yapkun17@gmail.com>
2023-05-14 21:36:24 +0800

    add table users migrations

--- migrations/1684071151100_create-table-users.js
+++ migrations/1684071151100_create-table-users.js
@@ -1,7 +1,25 @@
-/* eslint-disable camelcase */
+exports.up = (pgm) => {
+  pgm.createTable('users', {
+    id: {
+      type: 'VARCHAR(50)',
+      primaryKey: true,
+    },
+    username: {
+      type: 'VARCHAR(50)',
+      unique: true,
+      notNull: true,
+    },
+    password: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    fullname: {
+      type: 'TEXT',
+      notNull: true,
+    },
+  });
+};
 
-exports.shorthands = undefined;
-
-exports.up = pgm => {};
-
-exports.down = pgm => {};
+exports.down = (pgm) => {
+  pgm.dropTable('users');
+};
