df04c2b20642860e10c14c3f03adf53d42301ba9
mfikriam <yapkun17@gmail.com>
2023-05-28 19:16:30 +0800

    export notes implemented

--- src/server.js
+++ src/server.js
@@ -26,6 +26,11 @@ const collaborations = require('./api/collaborations');
 const CollaborationsService = require('./services/postgres/CollaborationsService');
 const CollaborationsValidator = require('./validator/collaborations');
 
+// ? Exports
+const _exports = require('./api/exports');
+const ProducerService = require('./services/rabbitmq/ProducerService');
+const ExportsValidator = require('./validator/exports');
+
 // ? Exceptions Types
 const ClientError = require('./exceptions/ClientError');
 
@@ -99,6 +104,13 @@ const init = async () => {
         collaborationsService,
         notesService,
         validator: CollaborationsValidator,
+      },
+    },
+    {
+      plugin: _exports,
+      options: {
+        service: ProducerService,
+        validator: ExportsValidator,
       },
     },
   ]);
